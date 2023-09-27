510c4099f435df37a65109482018f3d3dd22816a
unknown <ledmanas@gmail.com>
2023-09-28 00:16:41 +0300

    pridetas my_lib.h failas ir aprasytas basic duomenu ivedimas. reik patikrint vidurkio skaiciavima

---
+++ my_lib.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout; 
+using std::string;
+using std::cin;
+using std::endl;
+using std::vector;
+
+struct Studentas {
+  string vard, pav;
+  vector <int> paz;
+  int egz;
+  float rez;
+
+  
+};
+
+
+
+
+void mano_funkcija();
