003ee62dcc58aaa6e22162f45516345b5d5c25fd
unknown <ledmanas@gmail.com>
2023-09-28 03:03:40 +0300

    3 am shenaningans

--- my_lib.h
+++ my_lib.h
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
+#include <algorithm>
 
+using std::vector;
+using std::string;
+using std::sort;
+using std::cin;
+using std::cout;
+using std::setprecision;
 
-using namespace std;
 
 struct Studentas {
   string vard, pav;
   vector <int> paz;
   int egz;
   float rez;
-
-
 };
-
-
-
-
-void mano_funkcija();
