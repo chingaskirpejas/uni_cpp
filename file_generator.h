b968d71a36d4867603c118d1adc2c80507955cc5
Daniilas <ledmanas@gmail.com>
2023-10-29 11:43:53 +0200

    final changes

--- file_generator.h
+++ file_generator.h
@@ -1,4 +1,5 @@
 #include "my_lib.h"
+#include "functions.h"
 #ifndef ZUBA_FILE_GENERATOR_H
 #define ZUBA_FILE_GENERATOR_H
 vector <string> read_surenames();
@@ -6,4 +7,7 @@ vector <string> read_names();
 void generate_files();
 void create_x(int amount, int paz_kiek);
 void run_test(int amount, int paz_kiek);
+vector <Studentas> read_file(int kiekis, int nd_dydis);
+void atrinkimas(vector <Studentas>& visi_studentai, vector <Studentas>& vargsiukai, vector <Studentas>& kietekai);
+void write_to_file(int kiekis, vector <Studentas>& vargsiukai, vector <Studentas>& kietekai);
 #endif //ZUBA_FILE_GENERATOR_H
